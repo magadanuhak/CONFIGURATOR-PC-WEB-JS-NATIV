341a7aa97ba464a11f87003d7399b289b9c861b1
DANIEL <MAGADANUHAK>
2019-05-20 13:36:29 +0300

    Lcomit

---
+++ toGIT.bat
@@ -0,0 +1,11 @@
+echo "# CONFIGURATOR-PC-WEB-JS-NATIV" >> README.md
+git init
+pause
+git add README.md
+pause
+git commit -m "first commit"
+pause
+git remote add origin https://github.com/magadanuhak/CONFIGURATOR-PC-WEB-JS-NATIV.git
+pause
+git push -u origin master
+pause
